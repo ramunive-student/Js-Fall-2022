23bd058f2fe7604eed848462698b1ab234b9e76a
Roberta Munive <ramunive@gmail.com>
2022-10-12 14:44:19 -0500

    Included additional class practice exercises

--- Class1/ClassPracticeEx.js
+++ Class1/ClassPracticeEx.js
@@ -23,6 +23,28 @@ console.log(`\n\nIndex of 'w' (ignoring case) -> ${indexOf_w}`);
 
 
 
-console.log(`\n\nresult1 -> ${result1}`);
+
 
 const sentence2 = 'Pages That you vIeW in tHIs WinDOw wont apPeAr in the broWSer history AND thEY wont LEaVE other TRACes';
+
+let lastCharSentence2 = sentence2.substring(sentence2.length-1); 
+// OR sentence2.substring(sentence2.length-1, sentence2.length)
+console.log(`last character -> ${lastCharSentence2}`);
+console.log('\n\n')
+
+
+//-------------------------------------------------------------------
+
+let roomTemp = 63;
+
+//let heatStatus = roomTemp < 60 ? "turn heat on" : "turn heat off";
+// roomTemp > 70 ? "Turn AC on" : "Keep AC off" 
+// can use multiple ternary (combine them)
+
+let heatStatus = roomTemp <= 60 ? "Turn Heat On" : roomTemp > 70 ? "Turn AC on" : "Keep AC and Heat off"
+console.log(heatStatus);
+console.log('\n\n')
+
+//-----------------------------------------------------------------------
+
+
