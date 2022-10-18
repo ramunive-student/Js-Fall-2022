14b9a78fd4ad022bd860249d3f54f02f31e90585
Roberta Munive <ramunive@gmail.com>
2022-10-17 20:24:08 -0500

    Added additional class exercises to ClassPracticeEx.js

--- Class1/ClassPracticeEx.js
+++ Class1/ClassPracticeEx.js
@@ -47,4 +47,98 @@ console.log('\n\n')
 
 //-----------------------------------------------------------------------
 
+/**
+ * Add 'Mars' at index-0 (unshift)
+ * 
+ * Add 'Venus' at index-4
+ */
+ let planets = ['plUtO', 'URANUS', 'A', 'B', 'Saturn', 'JupiTEr', 'LOL', 'Mercury', 'EaRtH']
 
+ planets.unshift('Mars');
+ planets.splice(4, 0, 'Venus');
+
+ console.log(planets);  // or console.log(planets.toString())
+
+ //---------------------------------------------------------------------------
+
+ let dayName = '';
+
+ if (dayName.localeCompare('mon') === 0 || dayName.localeCompare('wed') === 0 || dayName.localeCompare('fri')===0){
+    console.log('attend the class');
+ } else if (dayName.localeCompare('tue')===0 || dayName.localeCompare('sat')===0){
+    console.log('attend lab session');
+ } else if (dayName.localeCompare('thu')===0){
+    console.log('self study');
+ } else if (dayName.localeCompare('sun')===0){  //can use else without the localeCompare if no possible error entry
+    console.log('Enjoy');
+ } else{
+    console.log('Enter valid day-name');
+ }
+
+ //-------------------------------------------------------------------------------------------------
+ let sports = ['football', 'Soccer', 'Baseball' , 'Rugby', 'Tennis'];
+ let lookFor = 'swimming';
+ if(sports.includes(lookFor)){
+    console.log(`Index of ${lookFor} is ${sports.indexOf(lookFor)}`);
+ } else{
+    sports.push(lookFor);
+ }
+
+ console.log(sports);
+
+ //-------------------------------------------------------------------------------------
+
+
+const sports1 = ['Football', 'Soccer', 'Baseball', 'Rugby', 'BASKETBALL', 'Ping pong'];
+
+for (let i=1 ; i < sports1.length ; i+=2) {
+    console.log(sports1[i]);
+}
+
+//________________________________________________________________________________
+
+/**
+ * sports = ['Football', 'Soccer', 'Baseball', 'Rugby', 'BASKETBALL', 'Ping pong'];
+/**
+ * print the value if either (value has an 'o') OR (value has length > 5 and less than 10)
+ * 
+ * Football
+ * Soccer
+ * Baseball
+ * Ping pong
+ * 
+ */
+
+
+ sports = ['Football', 'Soccer', 'Baseball', 'Rugby', 'BASKETBALL', 'Ping pong'];
+ let word = '';
+ console.log('\n\n*****************************');
+
+for (i = 0; i < sports.length; i++){
+
+   word = sports[i].toLowerCase();
+   
+
+   if (word.indexOf('o') != -1 || (word.length > 5 && word.length < 10)){
+      console.log(sports[i]);
+   }
+
+
+}
+console.log('\n\n*****************************');
+
+//______________________________________________________________________________________
+
+let reverse_sports = [];
+let temp = '';
+let counter = 0;
+for (i = sports.length - 1; i >= 0; i--){
+
+   temp = sports[i].toLowerCase();
+   reverse_sports[counter] = temp.charAt(0).toUpperCase() + temp.substring(1);
+   counter++;
+
+}
+console.log(`sports -> ${sports}`);
+console.log(`reverse_sports -> ${reverse_sports}`);
+console.log('\n\n*****************************');
