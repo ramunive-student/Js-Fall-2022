0f62235c5efaa5aa27a48809c9b7ab66aa8ea04c
Roberta Munive <ramunive@gmail.com>
2022-10-20 15:42:32 -0500

    Changed folder Class1 to ClassExercises

---
+++ ClassExercises/File1.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+console.log("It's a beautiful day!");
+
+/**
+ * In VS-Code, log is the shortcut for console.log()
+ */
+
+/**
+ * 1. Create a variable named myTeamName and assign any name to your team
+ * 2. Create a variable named myTeamSize and assign any number to it
+ * 3. Increment the team size by 1
+ * 4. Print the result like:   
+ *          I am part of ABC team, we have total X-members
+ */
+
+let myTeamName = 'All Stars';
+let myTeamSize = 5;
+myTeamSize++;
+console.log(`I am part of ${myTeamName} team, we have total ${myTeamSize}-members`);
+
+
